c0aa07911691272d9df24583b4e7b8645e416394
Alina Kazakova <alinakazakova@live.ie>
2018-10-26 12:52:58 +0100

    Transferred existing work

--- controller.py
+++ controller.py
@@ -2,7 +2,17 @@ class Controller:
 
     def __init__(self, model):
         self.model = model
+        self.numbersPressed = ""
 
     def button_pressed(self, button_label):
-        pass
+        if button_label ==  "C":
+            self.numbersPressed = ""
+            self.model.set_display("0")
+        elif button_label == "Del":
+            self.numbersPressed = button_label
+            self.model.set_display(numbersPressed)
+        else:
+            self.numbersPressed += button_label
+            self.model.set_display(self.numbersPressed)
+
 
--- controller_test.py
+++ controller_test.py
@@ -1,3 +1,39 @@
 import pytest
 from model import Model
 from controller import Controller
+
+@pytest.fixture
+def model():
+    return Model()
+
+@pytest.fixture
+def controller(model):
+    return Controller(model)
+
+def test_clear_button(model, controller):
+    #model = Model()
+    #controller = Controller(model)
+    controller.button_pressed("C")
+    assert "0" == model.display
+    # assert expected outcome == reference implementation
+
+def test_number_button (model, controller):
+    controller.button_pressed("7")
+    assert  "7" == model.display
+
+def test_digit_concatination(model, controller):
+    controller.button_pressed("4")
+    controller.button_pressed("5")
+    assert "45" == model.display
+
+def test_clear_digit_button(model, controller):
+    controller.button_pressed("5")
+    controller.button_pressed("C")
+    controller.button_pressed("6")
+    assert "6" == model.display
+
+def test_back_button(model, controller):
+    controller.button_pressed("8")
+    controller.button_pressed("Del")
+    controller.button_pressed("7")
+    assert "7" == model.display
